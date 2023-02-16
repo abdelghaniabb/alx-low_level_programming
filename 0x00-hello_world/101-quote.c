2ae3acf095b95bd57a6ef8550482f8baa2efb2b6
Abdelghani <abdelghani.aitbenbraim@gmail.com>
2023-02-17 00:18:55 +0100

    deposer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+const char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+const int length = sizeof(mes) - 1;
+write(2, mes, length);
+return (1);
+}
