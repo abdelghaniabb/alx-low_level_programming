aa77a06e1aa958afcaf548f3a7cc667bc9595911
Abdelghani <abdelghani.aitbenbraim@gmail.com>
2023-02-17 00:19:56 +0100

    deposer

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 const char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-const int length = sizeof(mes) - 1;
-write(2, mes, length);
+const int length = sizeof(m) - 1;
+write(2, m, length);
 return (1);
 }
