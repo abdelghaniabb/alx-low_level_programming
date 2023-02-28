188579ae40d9cfd3eb2332ed9f6f5faff935cd70
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-02-28 01:05:19 +0100

    test

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line
+ * print_array - prints n elements of an array of integers
  * @a: pointer to the first element of the array
  * @n: number of elements to print
  */
