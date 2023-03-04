6c0d4f3e1c4ef2729515e7d3cc1c05b75c816626
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-05 00:03:44 +0100

    add

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - chek code
+ * @c: integer
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (c == i)
+		{
+			printf("%d", 1);
+			break;
+		}
+	}
+	if (c != i)
+	{
+		return (0);
+	}
+}
