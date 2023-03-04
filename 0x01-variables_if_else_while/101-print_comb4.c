8eacbba494c12a2c40ae3544598ba20f405fffde
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-03-04 04:36:03 -0800

    testtt

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long i, j, k;
+i = '0';
+while (i <= '7')
+{
+	j = i + 1;
+	while (j <= '8')
+	{
+		k = j + 1;
+		while (k <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i != '7' || j != '8' || k != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			k++;
+		}
+		j++;
+	}
+	i++;
+}
+putchar('\n');
+return (0);
+}
