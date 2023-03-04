d30e40f76d78a7039986d72f1c1d7194e3d0d711
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-04 01:39:47 +0100

    test

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - upper
+ * @str: string
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (str);
+}
+
+
