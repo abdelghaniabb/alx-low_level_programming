e85594b1fa20b497b0846ca44c118b28c9291461
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-05 19:20:29 +0100

    test

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
+                return (1);
 		printf("%c", '+');
-		return (1);
 	}
 	else if (n == 0)
 	{
+                return (1);
 		printf("%c", '0');
-		return (0);
 	}
 	else
 	{
+                return (1);
 		printf("%c", '-');
-		return (-1);
 	}
 }
