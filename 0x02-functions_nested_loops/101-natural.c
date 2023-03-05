b65b179f3e94226cabd8e8d93bd3f61deb342c47
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-05 22:33:18 +0100

    test

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int s = 0;
+	int i = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			s = s + i;
+		}
+		i++;
+	}
+	printf("%d\n", s);
+}
