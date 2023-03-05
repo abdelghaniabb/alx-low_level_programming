2c9a46b8c9b14f0ce0eddac8d73c76941e4092e6
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-06 00:20:35 +0100

    test

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long i, n1, n2, n3;
+
+	n1 = 1;
+	n2 = 2;
+	i = 0;
+	while (i < 98)
+	{
+		if (i != 97)
+		{
+		printf("%ld, ", n1);
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+
+		}
+		else
+		{
+		printf("%ld\n", n1);
+		}
+		i++;
+	}
+	return (0);
+}
+
