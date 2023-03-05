d435893b3f948a44242bdb78b5691fab17aad8c8
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-06 00:00:02 +0100

    test

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -15,11 +15,18 @@ int main(void)
 	i = 0;
 	while (i < 50)
 	{
+		if (i != 49)
+		{
 		printf("%ld, ", n1);
 		n3 = n1 + n2;
 		n1 = n2;
 		n2 = n3;
 		i++;
+		}
+		else
+		{
+		printf("%ld", n1);
+		}
 	}
 	return (0);
 }
