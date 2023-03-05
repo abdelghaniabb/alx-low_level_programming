bce2676c07cb0072f0f1079f08599701597f97dd
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-05 21:56:40 +0100

    test

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,18 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n < 98)
 	{
 		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-		printf("%d", n);
+		printf("%d\n", n);
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n)
 	}
 	else
 	{
@@ -23,6 +27,6 @@ void print_to_98(int n)
 			printf("%d, ", n);
 			n--;
 		}
-		printf("%d", n);
+		printf("%d\", n);
 	}
 }
