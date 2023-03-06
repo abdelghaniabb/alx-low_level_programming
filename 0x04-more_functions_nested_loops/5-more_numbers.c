8430534be35c29274f63d3aab831674a692a4205
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-06 15:00:30 +0100

    test

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * more_numbers - check code
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+}
