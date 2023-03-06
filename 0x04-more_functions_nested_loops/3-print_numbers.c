8536bf67d017835dc1e110015f016f9b8e98539b
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-06 14:25:40 +0100

    test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_numbers - check code
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	char i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	printf("$\n");
+}
