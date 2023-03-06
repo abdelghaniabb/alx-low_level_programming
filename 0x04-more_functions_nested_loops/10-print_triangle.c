520d7696f4fc861dac560af4c0e97189b575871b
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-06 16:33:05 +0100

    test

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * print_triangle - check code
+ * @size: var
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i = 1, j, k;
+
+	while (i <= size && size > 0)
+	{
+		j = 1;
+		while (j <= size - i)
+		{
+			putchar(' ');
+			j++;
+		}
+		while (j <= size)
+		{
+			putchar('#');
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+}
