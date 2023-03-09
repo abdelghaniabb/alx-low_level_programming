3955588e97ff23a1efa408a2d85767deb8359106
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-09 18:47:53 +0100

    commit-task-5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * _sqrt_recursion - check code
  * @n: integer
@@ -5,5 +6,28 @@
  */
 int _sqrt_recursion(int n)
 {
-	
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	return (_sqrt_recursion_2(n, 1));
+}
+/**
+ * _sqrt_recursion_2 - check code
+ * @n: var1
+ * @i: parametre
+ * Return: 1 or 1
+ */
+int _sqrt_recursion_2(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return (_sqrt_recursion_2(n, i + 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,5 +4,6 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int _sqrt_recursion_2(int n, int i);
 int is_prime_number(int n);
 int is_prime_number_2(int n, int i);
