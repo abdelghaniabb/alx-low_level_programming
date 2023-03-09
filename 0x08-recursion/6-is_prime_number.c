96c080935816fdd066ba7e15cdfad04b8e69e3d4
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-09 17:59:07 +0100

    commit-task6-correction-2

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,43 @@
 /**
+ * is_prime_number_2 - check code
+ * @n: var
+ * @i: var2
+ * Return: 0 or 1
+ */
+
+int is_prime_number_2(int n, int i)
+{
+	if (n <= 2)
+	{
+		if (n == 2)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (i * i > n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_number_2(n, i + 1));
+	}
+}
+/**
  * is_prime_number - check code
  * @n: var
  * Return: 0 or 1
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-
-	for (i = 2; i < n; i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-	return (1);
+	return (is_prime_number_2(n, 2));
 }
+
