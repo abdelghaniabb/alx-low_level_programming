cba783cd9fdef89839dc16c8154f92652f873667
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-14 16:49:21 +0100

    first-correction2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,9 +10,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *arr;
 
-	if (size == 0 )
+	if (size == 0)
+	{
 		return (NULL);
-
+	}
 	arr = (char *) malloc(size * sizeof(char));
 	if (arr == NULL)
 	{
