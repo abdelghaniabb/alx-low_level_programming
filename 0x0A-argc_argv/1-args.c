9e6a417fbcbd146c79e5d787497bfb50a01fac3c
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-15 00:56:56 +0100

    task1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,14 @@
-#
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argv: An array containing the program command line arguments
+ * @argc: size of argv
+ * Return: void
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
