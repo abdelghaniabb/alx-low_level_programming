133cf6aa77d9e229de0db1b5bf20123b03eab1ac
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-15 00:31:55 +0100

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argv: An array containing the program command line arguments
+ * @argc: size of argv
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 ## 0x0B. C - malloc, free
 
  3-alloc_grid.c
+ 4-free_grid.c
