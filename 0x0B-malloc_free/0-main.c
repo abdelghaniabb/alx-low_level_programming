6555cf6f4f1c240a2ebb103ee52fcb326c6afe21
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-14 17:17:55 +0100

    commit-task2

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -35,23 +35,17 @@ void simple_print_buffer(char *buffer, unsigned int size)
  *
  * Return: Always 0.
  */
-/**
- * main - check the code .
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	char *buffer;
-	unsigned int size;
+    char *buffer;
 
-	size = 0;
-	buffer = create_array(size, 'H');
-	if (buffer == NULL)
-	{
-		printf("failed to allocate memory\n");
-		return (1);
-	}
-	free(buffer);
-	return (0);
+    buffer = create_array(98, 'H');
+    if  (buffer == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    simple_print_buffer(buffer, 98);
+    free(buffer);
+    return (0);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - duplicate of the string
+ * @str: string
+ * Return: new string
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	char *new_str = (char *) malloc((strlen(str) + 1) * sizeof(char));
+
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(new_str, str);
+	return (new_str);
+}
