b7de22c01d56fe809c11a3c556eb3d1bdbe3a7a4
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-15 01:25:08 +0100

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check code
+ * @argv: An array containing the program command line arguments
+ * @argc: size of argv
+ * Return: void
+ */
+
+int main(int argc, char **argv)
+{
+	int res;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	res = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", res);
+}
