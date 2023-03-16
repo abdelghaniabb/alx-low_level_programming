790798b89a909bd395c24fb07d1b9581c48b2d9a
abdo-aitbraim <abdobenbraim00011@gmail.com>
2023-03-16 19:38:53 +0100

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - cc
+ * @b: size
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = (void *)malloc(b);
+	if (ptr == NULL)
+	{
+		perror("malloc");
+		exit(98);
+	}
+	return (ptr);
+}
