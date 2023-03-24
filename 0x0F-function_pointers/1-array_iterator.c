d78782745450ea0c4c17d365ab5be1a183909928
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-03-24 21:45:26 +0000

    task1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * array_iterator - print a number
+ * @array: array of numbers
+ * @size: len of array
+ * @action: function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	if (action == NULL || array == NULL)
+		return;
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
