3bf4667f77645c3c7de8be60881ed3cec92128d8
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-03-30 22:16:18 +0000

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - add node in the list
+ * @head: the first element of the list
+ * @str: the string to add
+ * Return: list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	int n = strlen(str);
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = n;
+	new->next = *head;
+	*head = new;
+}
