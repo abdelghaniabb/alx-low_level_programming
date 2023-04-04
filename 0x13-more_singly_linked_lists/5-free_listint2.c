f130684092e55a22abbbf6afc60743c2660ae9a3
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-04 23:10:09 +0000

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free the list
+ * @head: the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *h;
+
+	while (*head)
+	{
+		h = *head;
+		*head = h->next;
+		free(h);
+	}
+	*head = NULL;
+}
