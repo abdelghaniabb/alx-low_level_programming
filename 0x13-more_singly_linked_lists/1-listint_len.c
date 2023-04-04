270f0b246312523683d3551b06b9082151e9e19e
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-04 21:45:45 +0000

    task1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+/**
+ * listint_len - return the number of elements in a list
+ * @h: the head of the list
+ * Return: the number of elemnts in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++:
+	}
+	return (i);
+}
