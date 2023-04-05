97fa7aa8a0ea7bff20c077105eb8e30c5bcc2603
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-05 15:08:49 +0000

    task9-c2

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,7 +33,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_n;
 		return (new_n);
 	}
-	else
+	if (*head == NULL && idx != 0)
 		return (NULL);
 
 	while (i < idx - 1 && h->next != NULL)
