9e20ec1d9ba93f67523de503c943b088f27616c1
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-07 23:12:59 +0000

    task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * binary_to_uint - from binary to disimal
+ * @b: the string of char  of binary
+ * Return:  unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int n = strlen(b);
+	int i = 0;
+	unsigned int res = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (i < n)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		if (b[i] == '1')
+			res = res + _pow(2, n - 1 - i);
+
+		i++;
+	}
+
+	return (res);
+}
+
+/**
+ * _pow - chek code
+ * @x: base
+ * @y: puissance
+ * Return: x^y
+ */
+
+int _pow(int x, int y)
+{
+	int res = 1;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y > 0)
+	{
+		res = x * _pow(x, y - 1);
+		return (res);
+	}
+	else
+	{
+		return (res);
+	}
+}
