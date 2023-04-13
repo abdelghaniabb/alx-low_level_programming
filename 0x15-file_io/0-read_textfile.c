47dbb57941b9c6440bd6ca486956d15f782dc74d
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-04-13 23:15:11 +0000

    task0-c1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,20 +1,50 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
-#include "main.h"
-
+/**
+ * read_textfile - read file
+ * @filename: name of the file to open
+ * @letters: number of letters to print
+ * Return: numberof letters printed
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *file = fopen (filename, "r");
-	char buffer[letters];
-	ssize_t n;
+	int fileDescriptor;
+	char *buffer;
+	ssize_t bytesRead;
+	ssize_t bytesWritten;
 
 	if (filename == NULL)
 		return (0);
 
-	if (file == NULL)
+	fileDescriptor = open(filename, O_RDONLY);
+	if (fileDescriptor == -1)
 		return (0);
 
-	n = fread(buffer, sizeof(char), letters, file);
-	fwrite(
-	return (n);
+	buffer = (char *)malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fileDescriptor);
+		return (0);
+	}
+
+	bytesRead = read(fileDescriptor, buffer, letters);
+	if (bytesRead <= 0)
+	{
+		close(fileDescriptor);
+		free(buffer);
+		return (0);
+	}
+
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten != bytesRead)
+	{
+		close(fileDescriptor);
+		free(buffer);
+		return (0);
+	}
+
+	close(fileDescriptor);
+	free(buffer);
+	return (bytesRead);
 }
