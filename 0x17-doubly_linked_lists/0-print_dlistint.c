00f01c681c998f08f75dca51022f65608299c6af
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-05-19 17:48:33 +0100

    task0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of the list
+ * @h: the head of the list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curent;
+	size_t count;
+
+	if (h == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+
+	curent = malloc(sizeof(dlistint_t));
+	if (curent == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+
+	curent = h;
+	while (curent)
+	{
+		printf("%d\n", curent->n);
+		curent = curent->next;
+		count++;
+	}
+
+	return (count);
+}
