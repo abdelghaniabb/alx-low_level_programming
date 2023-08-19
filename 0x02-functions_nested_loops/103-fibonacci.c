31de8559a993682f1f390ab08bfeb6ccd300060b
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-08-20 00:42:49 +0100

    fghj

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 1, j = 2, p = 2;
+
+	printf("%i, ", i);
+	while (i < 4000000)
+	{
+		if (i != 1)
+			p = i;
+		i = j + i;
+		j = p;
+		printf("%i, ", i);
+
+	}
+
+}
