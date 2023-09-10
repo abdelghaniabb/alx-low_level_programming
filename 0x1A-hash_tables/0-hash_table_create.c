db453e0322b25d8667e28255456d091dcc3d66d6
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-09-10 19:46:28 +0100

    task1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the array
+ * Return: pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned int size)
+{
+	hash_table_t *new;
+	unsigned int i;
+
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->array = malloc(sizeof(hash_table_t) * size);
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		new->array[i] = NULL;
+	}
+
+	return (new);
+}
